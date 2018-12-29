02f59d3b39a9b271f298c1db4a7aec4e645c0e0c
Avinash <avinashb97@gmail.com>
2018-12-30 04:33:52 +0530

    Adds Validation for the user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userController = require('../controllers/user');
+const validator = require('../controllers/validators/userValidators');
 
-router.post('/register', userController.register);
+router.post('/register', validator.register, userController.register);
 
 module.exports = router;
