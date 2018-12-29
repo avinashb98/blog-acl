0fe4fd52eb5885bd668631aa11efc22670546664
Avinash <avinashb97@gmail.com>
2018-12-30 04:27:17 +0530

    Fix EsLint Indentation

--- src/controllers/utils.js
+++ src/controllers/utils.js
@@ -1,11 +1,9 @@
 const generator = require('generate-password');
 
-const generatePassword = () => {
-  return generator.generate({
-    length: 10,
-    numbers: true
-  });
-};
+const generatePassword = () => generator.generate({
+  length: 10,
+  numbers: true
+});
 
 module.exports = {
   generatePassword
